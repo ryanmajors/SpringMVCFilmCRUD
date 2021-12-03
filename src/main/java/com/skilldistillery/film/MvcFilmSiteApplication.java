50beaab9f073e00f29b505670b2d09024ebdd6b9
Ryan Majors <growbox.ryan@yahoo.com>
2021-12-03 15:47:15 -0700

    Stub project

---
+++ src/main/java/com/skilldistillery/film/MvcFilmSiteApplication.java
@@ -0,0 +1,13 @@
+package com.skilldistillery.film;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MvcFilmSiteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MvcFilmSiteApplication.class, args);
+	}
+
+}
